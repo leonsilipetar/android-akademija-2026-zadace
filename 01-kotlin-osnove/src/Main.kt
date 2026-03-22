eee5d50d53a9f5501ae1985365408ecf86a51008
leonsilipetar <leon.silipetar@gmail.com>
2026-03-22 12:01:48 +0100

    Initial commit - setup repo and Kotlin module

---
+++ 01-kotlin-osnove/src/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    zadatak1()
+    zadatak2()
+    zadatak3()
+    zadatak4()
+    zadatak5()
+}
+
+fun zadatak1(){
+
+}
+
+fun zadatak2(){}
+
+fun zadatak3(){}
+
+fun zadatak4(){}
+
+fun zadatak5(){}
