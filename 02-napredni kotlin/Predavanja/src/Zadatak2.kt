f6d6974b23188dfb4b2843e66b90ec5a0aca69ee
leonsilipetar <leon.silipetar@gmail.com>
2026-03-28 18:07:10 +0100

    Remove Predavanja folder from tracking

--- 02-napredni kotlin/Predavanja/src/Zadatak2.kt
+++
@@ -1,9 +0,0 @@
-//funkcijski tipovi
-fun main(){
-    operate(2, 2, {a, b -> a + b})
-    operate(2, 2, {a, b -> a * b})
-    operate(2, 2, {a, b -> if(a > b) a else b })
-}
-fun operate(a: Int, b: Int, operation: (Int, Int) -> Int){
-    println( operation(a, b) )
-}
